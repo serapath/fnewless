c85bc9291c7f9e3b432334c3345326075457a66d
serapath <dev@serapath.de>
2018-02-05 19:19:11 +0800

    REFACTOR (minimize) fnewless

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ module.exports = fnewless
 
 function fnewless (ctor) {
   // @TODO: enable objects/functions to inherit from other objects/functions
-  function CTOR () {
-    if (typeof ctor.prototype === 'function') {
-      var self = function () { return CTOR.prototype.apply(self, arguments) }
-      self.__proto__ = CTOR.prototype
-      Object.defineProperty(self, 'constructor', ctorProp)
-    } else {
-      var self = Object.create(CTOR.prototype, { constructor: ctorProp })
-    }
-    ctor.apply(self, arguments)
-    return self
-  }
-  const ctorProp = {
-    value: CTOR,
-    enumerable: false,
-    writable: true,
-    configurable: true
+  const CTOR = (typeof ctor.prototype === 'function') ? function CTOR () {
+    const self = function () { return ctor.prototype.apply(self, arguments) }
+    self.__proto__ = ctor.prototype
+    return finalize(self, arguments)
+  } : function CTOR () {
+    const self = Object.create(ctor.prototype)
+    return finalize(self, arguments)
   }
   CTOR.prototype = ctor.prototype
   return CTOR
+  function finalize (self, args) {
+    Object.defineProperty(self.__proto__, 'constructor', {
+      value: CTOR, enumerable: false, writable: true, configurable: true
+    })
+    ctor.apply(self, args)
+    return self
+  }
 }
